a8702c5a732393ab5e135e2f7e145e6ec40c4b6e
Shahzeb-Abro <khizr772@gmail.com>
2025-07-19 16:19:11 +0500

    Added socket basic setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
-const app = express();
+import { createServer } from "node:http";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./utils/db.js";
 import "colors";
+import { Server } from "socket.io";
 dotenv.config();
 
 import passport from "passport";
@@ -13,6 +14,19 @@ import cookieParser from "cookie-parser";
 
 import globalErrorHandler from "./controllers/error.controller.js";
 import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
 
 app.use(passport.initialize());
 
@@ -39,6 +53,6 @@ app.use("/api/v1/auth", authRoutes);
 
 app.use(globalErrorHandler);
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
